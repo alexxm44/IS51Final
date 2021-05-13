14ae590b96097a55ad9cdf3b246c923bc2b080e0
Alex M <Alex@Alexs-Air.attlocal.net>
2021-05-13 16:41:27 -0700

    finish structured english

---
+++ is51_final.py
@@ -0,0 +1,6 @@
+"""
+The program is trying to display the number of grades, the average grade, and the percentage of grades shown from the list in txt file
+divide the total by the sum of the grades that are provided in that file
+find out how many values are above the average grade. divide that number by the how many values there are.
+configure a percentage and then round it
+"""
