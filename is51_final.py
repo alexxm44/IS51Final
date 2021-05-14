d8db586eeec446ef19270a0dc1323625223e63c0
Alex M <Alex@Alexs-Air.attlocal.net>
2021-05-13 17:00:36 -0700

    finish pseudo code

--- is51_final.py
+++ is51_final.py
@@ -4,3 +4,20 @@ divide the total by the sum of the grades that are provided in that file
 find out how many values are above the average grade. divide that number by the how many values there are.
 configure a percentage and then round it
 """
+
+"""
+main():
+    open the txt as an infile
+    allocte the lines of the list
+    close the code file
+    calculate_percent_above_average()
+calculate_percent_above_average():
+number of lines = len()
+total = total()
+average = total/number of lines
+iterate through the lines:
+if it is above the average: 
+    number of values = number of values + 1
+
+percent above average = above average + number of values
+"""
